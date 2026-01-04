7685bcc29c4d63efa7a4a63c627edbac69647ead
24tx610002-VoVanNgoc <24tx610002@student.hcmute.edu.vn>
2026-01-04 20:57:12 +0700

    test

--- src/main/java/vn/orishop/utils/EmailUtils.java
+++ src/main/java/vn/orishop/utils/EmailUtils.java
@@ -6,34 +6,31 @@ import jakarta.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class EmailUtils {
-    // Thay đổi thông tin của bạn ở đây
-    private static final String HOST_NAME = "smtp.gmail.com";
-    private static final int SSL_PORT = 465;
-    private static final String APP_EMAIL = "ngocvosr4@gmail.com"; // Email của bạn
-    private static final String APP_PASSWORD = "rksi dfbz mbdz huor"; // Mật khẩu ứng dụng Gmail
+	// Thay đổi thông tin của bạn ở đây
+	private static final String HOST_NAME = "smtp.gmail.com";
+	private static final int SSL_PORT = 465;
+	private static final String APP_EMAIL = "ngocvosr4@gmail.com"; // Email của bạn
+	private static final String APP_PASSWORD = "rksi dfbz mbdz huor"; // Mật khẩu ứng dụng Gmail
 
-    public static void send(String to, String subject, String body) throws Exception {
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.host", HOST_NAME);
-        props.put("mail.smtp.socketFactory.port", SSL_PORT);
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.port", SSL_PORT);
+	public static void send(String to, String subject, String body) throws Exception {
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.host", HOST_NAME);
+		props.put("mail.smtp.socketFactory.port", SSL_PORT);
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.port", SSL_PORT);
 
-        Session session =
-                Session.getDefaultInstance(
-                        props,
-                        new Authenticator() {
-                            protected PasswordAuthentication getPasswordAuthentication() {
-                                return new PasswordAuthentication(APP_EMAIL, APP_PASSWORD);
-                            }
-                        });
+		Session session = Session.getDefaultInstance(props, new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(APP_EMAIL, APP_PASSWORD);
+			}
+		});
 
-        MimeMessage message = new MimeMessage(session);
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-        message.setSubject(subject, "UTF-8");
-        message.setContent(body, "text/html; charset=UTF-8");
+		MimeMessage message = new MimeMessage(session);
+		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+		message.setSubject(subject, "UTF-8");
+		message.setContent(body, "text/html; charset=UTF-8");
 
-        Transport.send(message);
-    }
+		Transport.send(message);
+	}
 }
